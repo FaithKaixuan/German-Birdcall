1f049f42b57435ad93a65633207f8b58a1839353
FaithKaixuan <1102093868@qq.com>
2021-03-20 11:54:58 +0100

    Update Germany_Bird_Code_generation.py

--- Germany_Bird_Code_generation.py
+++ Germany_Bird_Code_generation.py
@@ -4,7 +4,7 @@ train_csv = pd.DataFrame(columns = ["gen","sp","filename"])
 path = "/mnt/GermanyBirdcall/Germany_Birdcall_resampled"
 primary_labels = os.listdir( path )
 i = 0
-BIRD_CODE = {}
+BIRD_CODE = []
 for gen in primary_labels:
   BIRD_CODE.append({gen: i})
   i = i + 1
